70db4a8a570411f2c9db4fb8b8235d7798d32f32
djohnson0610 <djohnson0610@hotmail.com>
2014-10-11 12:40:56 -0700

    Sessions routes created for log in/log out/new session.

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,7 @@ Rails.application.routes.draw do
   resources :tests do
     resources :questions
   end
+  post "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  post "sessions/create" => "sessions#create"
 end
