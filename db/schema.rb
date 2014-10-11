5d8222e18f34ea61b0ecd6b6523003b9a23d6797
Ziad <mr.ziad.abdo@gmail.com>
2014-10-11 15:45:46 -0700

    *login page has path for teacher     and student signup   *student and teacher signup fields     are correct   *added javascript for hidden buttons

--- db/schema.rb
+++ db/schema.rb
@@ -81,4 +81,11 @@ ActiveRecord::Schema.define(version: 20141011190940) do
     t.datetime "updated_at"
   end
 
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
