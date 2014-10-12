47c1f58b3eed3be9a72379fe753d3892013f630f
djohnson0610 <djohnson0610@hotmail.com>
2014-10-11 18:18:56 -0700

    Added Bcrypt to gemfile
    Added current_user helper to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  private
+  def current_user
+    @current_user ||= Teacher.find(session[:id]) if session[:type] == "teacher" || Student.find(session[:id]) if session[:type] == "student"
+  end
 end
