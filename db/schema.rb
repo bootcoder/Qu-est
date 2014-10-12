3be3643d1213b5e642756c76008618a5a8cbdf5a
djohnson0610 <djohnson0610@hotmail.com>
2014-10-11 18:36:06 -0700

    Added password encryption fields to Students. Added password encryption methods to Student model. Allowed password params to be used in Student controller. Added password and password confirmation fields to new student form partial.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011190940) do
+ActiveRecord::Schema.define(version: 20141012012755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20141011190940) do
     t.string   "email"
     t.string   "phone"
     t.string   "password"
+    t.string   "password_salt"
+    t.string   "password_hash"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -79,6 +81,8 @@ ActiveRecord::Schema.define(version: 20141011190940) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_salt"
+    t.string   "password_hash"
   end
 
 end
