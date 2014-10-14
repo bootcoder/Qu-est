c9c22f6cd5dbc6d210422f333973c812c6b2ec1b
Hunter Chapman <bootcoder@gmail.com>
2014-10-14 02:09:15 -0700

    add flash notice on logout -- fix spelling error in flash notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,18 +9,19 @@ class SessionsController < ApplicationController
     if student
       session[:id] = student.id
       session[:type] = "student"
-      flash[:notice] = "Login as a student was sucessful"
+      flash[:notice] = "Login as a student was successful"
     elsif teacher
       session[:id] = teacher.id
       session[:type] = "teacher"
-      flash[:notice] = "Login as a teacher was sucessful"
+      flash[:notice] = "Login as a teacher was successful"
     else
-      flash[:notice] = "Login was unsucessful"
+      flash[:notice] = "Login was unsuccessful"
     end
       redirect_to root_path
   end
 
   def destroy
+    flash[:notice] = "You have been logged out."
     session[:id] = nil
     session[:type] = nil
     redirect_to root_path
